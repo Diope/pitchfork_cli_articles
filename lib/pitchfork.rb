d332b36543321f2d041e9a2ad323c7b4f94875fb
Dion <kalani.pettaway@gmail.com>
2015-05-10 23:43:52 -0400

    Pitchfork CLI

--- lib/pitchfork.rb
+++ lib/pitchfork.rb
@@ -3,14 +3,14 @@ require "open-uri"
 
 class Pitchfork
 
-	attr_accessor :doc, :list
+	attr_accessor :site, :list
 
 	def initialize
 		# Create a i.variable that opens pitchfork
-		@doc = Nokogiri::HTML(open("http://pitchfork.com/"))
+		@site = Nokogiri::HTML(open("http://pitchfork.com/"))
 		# list will go through doc's (the site's) css looking for certain divs
 		# that help direct it to the correct article/article headers
-		@list = doc.css("div#page div#content div#homepage div.homepage-column div#hp-blog-box ul li a")
+		@list = site.css("div#page div#content div#homepage div.homepage-column div#hp-blog-box ul li a")
 	end
 
 	def the_pitch
@@ -19,23 +19,24 @@ class Pitchfork
 		@list.each_with_index {|article, i| puts "#{i+1}. #{article.text.strip}"}
 	end
 
-	def open_story(index)
+	def open_article(index)
+		root_path = "http://pitchfork.com"
 		# convert index to an integer and index is less or eqal to list length then set the variable link
 		# to list index with the href attirbute from headlines, this opens the link
 		if index.to_i > 0 && index.to_i <= list.length
-			article_link = list[index.to_i-1].attribute("href").value
+			article_link = root_path << list[index.to_i-1].attribute("href").value
 			system "open #{article_link}"
 		end
 	end
 
-	def call
+	def today_on
 		# Self explanatory really
 		puts "Today on The Pitch via Pitchfork: "
 		the_pitch
 		puts "\nWhich story would you like to read?"
 		i = gets.strip
-		open_story(i)
+		open_article(i)
 	end
 end
 
-Pitchfork.new.call
+Pitchfork.new.today_on
